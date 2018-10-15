ab3547ff2e6579d64b4e32ab9a39db49d960ddb4
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2018-10-15 09:46:24 +0530

    Implement Battery Theme
    This theme is based on paradox theme, but also shows battery charge
    next to time.

---
+++ Battery.psm1
@@ -0,0 +1,99 @@
+#requires -Version 2 -Modules posh-git
+
+function Write-Theme {
+    param(
+        [bool]
+        $lastCommandFailed,
+        [string]
+        $with
+    )
+
+    $lastColor = $sl.Colors.PromptBackgroundColor
+    $prompt = Write-Prompt -Object $sl.PromptSymbols.StartSymbol -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+
+    #check the last command state and indicate if failed
+    If ($lastCommandFailed) {
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.FailedCommandSymbol) " -ForegroundColor $sl.Colors.CommandFailedIconForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+    }
+
+    #check for elevated prompt
+    If (Test-Administrator) {
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.ElevatedSymbol) " -ForegroundColor $sl.Colors.AdminIconForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+    }
+
+    $user = [System.Environment]::UserName
+    $computer = [System.Environment]::MachineName
+    $path = Get-FullPath -dir $pwd
+    if (Test-NotDefaultUser($user)) {
+        $prompt += Write-Prompt -Object "$user@$computer " -ForegroundColor $sl.Colors.SessionInfoForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+    }
+
+    if (Test-VirtualEnv) {
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.SegmentForwardSymbol) " -ForegroundColor $sl.Colors.SessionInfoBackgroundColor -BackgroundColor $sl.Colors.VirtualEnvBackgroundColor
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.VirtualEnvSymbol) $(Get-VirtualEnvName) " -ForegroundColor $sl.Colors.VirtualEnvForegroundColor -BackgroundColor $sl.Colors.VirtualEnvBackgroundColor
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.SegmentForwardSymbol) " -ForegroundColor $sl.Colors.VirtualEnvBackgroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+    }
+    else {
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.SegmentForwardSymbol) " -ForegroundColor $sl.Colors.SessionInfoBackgroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+    }
+
+    # Writes the drive portion
+    $prompt += Write-Prompt -Object "$path " -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+
+    $status = Get-VCSStatus
+    if ($status) {
+        $themeInfo = Get-VcsInfo -status ($status)
+        $lastColor = $themeInfo.BackgroundColor
+        $prompt += Write-Prompt -Object $($sl.PromptSymbols.SegmentForwardSymbol) -ForegroundColor $sl.Colors.PromptBackgroundColor -BackgroundColor $lastColor
+        $prompt += Write-Prompt -Object " $($themeInfo.VcInfo) " -BackgroundColor $lastColor -ForegroundColor $sl.Colors.GitForegroundColor
+    }
+
+    # Writes the postfix to the prompt
+    $prompt += Write-Prompt -Object $sl.PromptSymbols.SegmentForwardSymbol -ForegroundColor $lastColor
+
+    # Write battery charge remaining
+    $remainingCharge = (Get-WmiObject -Class Win32_Battery).estimatedchargeremaining
+
+    $timeStamp = Get-Date -UFormat %r
+    $timestamp = "[$timeStamp]"
+
+    $rightSideLength = $timestamp.Length + $remainingCharge.Length + 2
+    $prompt += Set-CursorForRightBlockWrite -textLength $rightSideLength
+    $prompt += Write-Prompt $timeStamp -ForegroundColor $sl.Colors.PromptForegroundColor
+    $promp += Write-Prompt " "
+
+    if($remainingCharge -ge 90){
+        $promp += Write-Prompt $remainingCharge -ForegroundColor $HighBattery
+    } elseif ($remainingCharge -ge 50) {
+        $promp += Write-Prompt $remainingCharge -ForegroundColor $MidBattery
+    } else {
+        $promp += Write-Prompt $remainingCharge -ForegroundColor $LowBattery
+    }
+
+    $prompt += Set-Newline
+
+    if ($with) {
+        $prompt += Write-Prompt -Object "$($with.ToUpper()) " -BackgroundColor $sl.Colors.WithBackgroundColor -ForegroundColor $sl.Colors.WithForegroundColor
+    }
+
+    $prompt += Write-Prompt -Object $sl.PromptSymbols.PromptIndicator -ForegroundColor $sl.Colors.PromptBackgroundColor
+    $prompt += ' '
+    $prompt
+}
+
+$sl = $global:ThemeSettings #local settings
+$sl.PromptSymbols.StartSymbol = ' '
+$sl.PromptSymbols.PromptIndicator = [char]::ConvertFromUtf32(0x276F)
+$sl.PromptSymbols.SegmentForwardSymbol = [char]::ConvertFromUtf32(0xE0B0)
+$sl.Colors.PromptForegroundColor = [ConsoleColor]::White
+$sl.Colors.PromptSymbolColor = [ConsoleColor]::White
+$sl.Colors.PromptHighlightColor = [ConsoleColor]::DarkBlue
+$sl.Colors.GitForegroundColor = [ConsoleColor]::Black
+$sl.Colors.WithForegroundColor = [ConsoleColor]::DarkRed
+$sl.Colors.WithBackgroundColor = [ConsoleColor]::Magenta
+$sl.Colors.VirtualEnvBackgroundColor = [System.ConsoleColor]::Red
+$sl.Colors.VirtualEnvForegroundColor = [System.ConsoleColor]::White
+$HighBattery = [ConsoleColor]::DarkBlue
+$MidBattery = [ConsoleColor]::Cyan
+$LowBattery = [ConsoleColor]::Red
+
